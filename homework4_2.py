f84ea5504a3e2a4801378b5a7d20a15e0713d890
Olha Karpenko <olhalilu@gmail.com>
2025-03-17 16:02:45 +0400

    changed homework4_2

--- homework4_2.py
+++ homework4_2.py
@@ -3,8 +3,28 @@ import re
 
 def normalize_sentence(my_string):
     my_string1 = my_string.lower()#lower case
-    my_string2 = my_string1.replace(' iz ',' is ')#replace  standalone iz
+    my_string2 = re.sub(r'(?<![“"])(?<!\w)\biz\b(?!\w)', 'is', my_string1)#replace  standalone iz
     return my_string2
+
+# def define_insert(my_string2):
+#     sentences = re.split(r'(?<=[.])', my_string2)
+#
+#     # last word of every sentences
+#     last_words = []
+#     for sentence in sentences:
+#         words = re.findall(r'\b\w+\b', sentence)
+#         if words:
+#             last_word = words[-1]
+#             last_words.append(last_word)
+#     word_after = 'paragraph.'
+#     insert_index = my_string2.lower().find('paragraph')
+#     sent_last_word = ' '.join(last_words)  #ready sentence
+#     sentence_before_insert = my_string2[:insert_index+len(word_after)]#where to insert new sentence
+#     sentence_after_insert = my_string2[insert_index+len(word_after):]#where continue the rest of the text after inserting new sentence
+#     new_sentence = sentence_before_insert + sent_last_word +"." + sentence_after_insert
+#     return new_sentence
+
+
 
 def define_insert(my_string2):
     sentences = re.split(r'(?<=[.])', my_string2)
@@ -16,14 +36,28 @@ def define_insert(my_string2):
         if words:
             last_word = words[-1]
             last_words.append(last_word)
-    word_after = 'paragraph.'
-    insert_index = my_string2.lower().find('paragraph')
-    sent_last_word = ' '.join(last_words)  #ready sentence
-    sentence_before_insert = my_string2[:insert_index+len(word_after)]#where to insert new sentence
-    sentence_after_insert = my_string2[insert_index+len(word_after):]#where continue the rest of the text after inserting new sentence
-    new_sentence = sentence_before_insert + sent_last_word +"." + sentence_after_insert
-    return new_sentence
 
+    # find word 'paragraph' and its position
+    word_to_find = 'paragraph.'
+    paragraph_index = my_string2.lower().find(word_to_find)  # Позиция слова "paragraph"
+
+    if paragraph_index == -1:
+        raise ValueError(f"'{word_to_find}' not found in the text!")
+
+    # word paragraph with all spaces and whitespaces
+    insert_index = paragraph_index + len(word_to_find)
+    while insert_index < len(my_string2) and my_string2[insert_index] in (' ', '\n', '\t'):
+        insert_index += 1
+
+    # sentence consists of last words
+    sentence_to_insert = ' '.join(last_words) + '.'
+
+    # insert new combined sentence
+    part_before = my_string2[:insert_index]
+    part_after = my_string2[insert_index:]
+    new_sentence = part_before + '\n' + sentence_to_insert + '\n' + part_after
+
+    return new_sentence
 
 def calculate_whitespaspaсes(new_sentence):
     symbol_counts = 0
